ca4f2c02b823bf1b1f6ebddf9201acf0a1005f9b
Carlos David Apolaya Mendoza <apolayamendozacarlos@gmail.com>
2024-05-29 16:41:41 -0500

    Se crea unos json para ir mostrando los resultados

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,40 @@
+import peliResult from './json/with-results.json'
+import noPelis from './json/not-results.json'
+import './App.css'
+
 const App = () => {
+  const pelicula = peliResult?.Search
+  const hasPelicula = pelicula?.length > 0
+
   return (
-    <div>Buscador de peliculas</div>
+    <div className="page">
+      <header>
+        <form>
+          <input type="text" placeholder="Pelicula"/>
+          <button type='submit'>
+            Enviar
+          </button>
+        </form>
+      </header>
+
+      <main>
+        {
+          hasPelicula ? (
+            <ul>
+              {pelicula.map((peli) => (
+                <li key={peli.imdbID}>
+                  <h2> {peli.Title} </h2>
+                  <p> {peli.Year} </p>
+                  <img src={peli.Poster} alt={peli.Title} />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <span> {JSON.stringify(noPelis)} </span>
+          )
+        }
+      </main>
+    </div>
   )
 }
 
